3f8b8970679947eb6f655cd25964040dd88802bb
itsumura-h <dumblepy@gmail.com>
2020-08-22 04:48:03 +0900

    update libs routing

--- src/application/libs/routing.py
+++ src/application/libs/routing.py
@@ -1,16 +1,37 @@
-from starlette.routing import Route
+from starlette.routing import Route as StarletteRoute
+
+class Route:
+    @staticmethod
+    def get(path:str, endpoint):
+        return StarletteRoute(path, endpoint=endpoint, methods=['GET'])
+
+    @staticmethod
+    def post(path:str, endpoint):
+        return StarletteRoute(path, endpoint=endpoint, methods=['POST'])
+
+    @staticmethod
+    def patch(path:str, endpoint):
+        return StarletteRoute(path, endpoint=endpoint, methods=['PATCH'])
+
+    @staticmethod
+    def put(path:str, endpoint):
+        return StarletteRoute(path, endpoint=endpoint, methods=['PUT'])
+
+    @staticmethod
+    def delete(path:str, endpoint):
+        return StarletteRoute(path, endpoint=endpoint, methods=['DELETE'])
 
 def get(path:str, endpoint):
-    return Route(path, endpoint=endpoint, methods=['GET'])
+    return StarletteRoute(path, endpoint=endpoint, methods=['GET'])
 
 def post(path:str, endpoint):
-    return Route(path, endpoint=endpoint, methods=['POST'])
+    return StarletteRoute(path, endpoint=endpoint, methods=['POST'])
 
 def patch(path:str, endpoint):
-    return Route(path, endpoint=endpoint, methods=['PATCH'])
+    return StarletteRoute(path, endpoint=endpoint, methods=['PATCH'])
 
 def put(path:str, endpoint):
-    return Route(path, endpoint=endpoint, methods=['put'])
+    return StarletteRoute(path, endpoint=endpoint, methods=['PUT'])
 
 def delete(path:str, endpoint):
-    return Route(path, endpoint=endpoint, methods=['DELETE'])
+    return StarletteRoute(path, endpoint=endpoint, methods=['DELETE'])
--- src/application/main.py
+++ src/application/main.py
@@ -1,14 +1,11 @@
-from libs.routing import (
-    get,
-    post
-)
+from libs.routing import Route
 from libs.server import run_server
 from app.controllers.user_controller import UserController
 
 routes = [
-    get('/users', UserController.index),
-    post('/users', UserController.create),
-    get('/users/{id}', UserController.show)
+    Route.get('/users', UserController.index),
+    Route.post('/users', UserController.create),
+    Route.get('/users/{id}', UserController.show)
 ]
 
 app = run_server(routes, debug=True)
